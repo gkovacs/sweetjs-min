33e3202f2469a890a1bc032b10e77ed5b0465d8b
Geza Kovacs <gkovacs@stanford.edu>
2017-02-24 02:09:32 -0800

    removed dependencies on babel-core

--- dist/node-module-resolver.js
+++ dist/node-module-resolver.js
@@ -5,10 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = resolveModule;
 
-var _resolve = require('resolve');
-
-var _resolve2 = _interopRequireDefault(_resolve);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function resolveModule(path, cwd) {
